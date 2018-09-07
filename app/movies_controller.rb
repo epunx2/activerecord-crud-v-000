3dd11eb439e11ecf01e7adf69f98cd8411d03db4
Eric Chrobak <eric@ericchrobak.com>
2018-09-07 00:48:31 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,7 +21,7 @@ def can_be_created_with_a_hash_of_attributes
       in_theaters: false
   }
   # binding.pry
-  movie = Movie.new(*attributes)
+  movie = Movie.new(**attributes)
 end
 
 def can_be_created_in_a_block(args = __)
